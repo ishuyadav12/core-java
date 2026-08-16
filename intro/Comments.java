af68c1aaad666c9ddd342c1a32ff646fdb57a4ff
Ishu <165940337+ishuyadav12@users.noreply.github.com>
2026-08-16 09:59:48 +0530

    Add Comments class with comment examples
    Introduced Comments class with examples of single-line, multi-line, and Javadoc comments.

---
+++ intro/Comments.java
@@ -0,0 +1,28 @@
+package com.intro;
+
+public class Comments {
+
+	public static void main(String[] args) {
+
+		// This is a single-line comment
+		System.out.println("Hello, World!");
+
+		/*
+		 * This is a multi-line comment.
+		 */
+		System.out.println("Hello, World!");
+
+	}
+
+	/**
+	 * Adds two numbers.
+	 * 
+	 * @param a First number
+	 * @param b Second number
+	 * @return Sum of a and b
+	 */
+	public int add(int a, int b) {
+		return a + b;
+	}
+
+}
